a35731d860c08975290a61938f7ffa7eeca6ec7c
kpinar <kpinar@seas.upenn.edu>
2015-05-26 23:12:42 +0200

    Classroom app

---
+++ apps/vclass/app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+class WelcomeController < ApplicationController
+  def index
+  	@welcome_index = "Greetings matey!"
+  end
+
+  def students
+  	@students = ["Kaan Pinar", "Matthew Biggins", "Danny Low", "Bailey Berro", 
+  		"Blake Altman", "Alex Schwartz", "Oscar Wang", "Asad Khan", "Arya Naik", 
+  		"Austin Rief", "Ben Pachas", "Enrique Pajuelo", "Charlotte Searle", 
+  		"Gloryah Allen", "Kate Ernst", "Molly Davey", "Olivia Bicks", 
+  		"Pavi Vetriselvan", "Sophie Tao", "Stanley Zuo", "Susana Yepes", 
+  		"Urvish Patel", "Yunny Chung", "Zack Hyman"]
+  end
+
+end
