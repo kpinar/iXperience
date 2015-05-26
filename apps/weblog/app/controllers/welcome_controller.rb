2639202c606f0e38ff7f9e64bd6827c585aa7882
kpinar <kpinar@seas.upenn.edu>
2015-05-26 14:22:14 +0200

    iXperience with rails weblog app

---
+++ apps/weblog/app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def index
+  	@name = "Kaan"
+  end
+
+  def about
+  end
+  
+end
---
+++ d6/exercise_6b.rb
@@ -0,0 +1,18 @@
+#E1
+#
+# 1. A request is an action performed by a client like a Browser or rest-client
+# towards a Server to gain information about a webpage. It's made via a hypertext transfer protocol
+#
+# 2. A client makes the request such as Chrome, Firefox etc.
+#
+# 3. It goes to a server, which is usually a bunch of big black boxes stored in a warehouse
+# It's on a hypertext transfer protocol. It's address is a domain name, a dns given by the ICAAP
+#
+# 4. It is approved by the server which sends back a response. Before that params are sent to
+# an application which gives back an html page
+#
+# 5. An html page that is hopefully user friendly comes back that you can interact with.
+#
+# 6. Your browser interacts with it and makes it look like the page you see and further 
+# communicates with it, that makes the website interactive.
+
